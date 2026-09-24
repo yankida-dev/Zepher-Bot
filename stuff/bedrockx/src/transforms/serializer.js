2d34e249904a6cbff4da605680fa24481dc5aca3
Yanki <yancilo37@gmail.com>
2026-09-23 18:43:08 -0700

    Create serializer.js

---
+++ stuff/bedrockx/src/transforms/serializer.js
@@ -0,0 +1,23 @@
+const { ProtoDefCompiler } = require('protodef').Compiler
+const { FullPacketParser, Serializer } = require('protodef')
+
+const protocol = require("../protocol/protocol.json")
+const compiler = new ProtoDefCompiler()
+
+compiler.addTypesToCompile(protocol.types)
+compiler.addTypes(require('../datatypes/compiler-minecraft'))
+
+const proto = compiler.compileProtoDefSync()
+
+const PROTOCOL_VERSION = 2193
+const GAME_VERSION = '1.26.50'
+
+function createSerializer() {
+  return new Serializer(proto, 'mcpe_packet')
+}
+
+function createDeserializer() {
+  return new FullPacketParser(proto, 'mcpe_packet')
+}
+
+module.exports = { createDeserializer, createSerializer, PROTOCOL_VERSION, GAME_VERSION }
